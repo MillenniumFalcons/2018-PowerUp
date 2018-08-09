ea3228479810871be0dbf62e698c2c02df21cd67
Surya Madhan <suryamadhan@gmail.com>
2018-08-09 15:07:37 -0400

    Addings steps 1-3 for wrist

--- Robot_Code/src/team3647elevator/Wrist.java
+++ Robot_Code/src/team3647elevator/Wrist.java
@@ -1,4 +1,22 @@
 public class Wrist 
 {
+	public static int elevatorState, aimedElevatorState;
+	/*
+	 * 0. Start
+	 * 1. Flat
+	 * 2. Sixty-Degrees
+	 * 3. Facing Up
+	 */
+
+	public static boolean start, flat, sixtyDegrees, up, manualOverride, originalPositionButton;
+	public static double overrideValue, speed;
+
+	//Insert step 1 code below
 	
+
+	//Insert step 3 code below
+	public static void setLimitSwitch()
+	{
+		flat = limitSwitch.get();
+	}
 }
