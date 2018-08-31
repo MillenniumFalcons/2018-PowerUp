9bc40a765ef20ec7c38b2f4f7267b4359a7d7b86
farrahkaiyom <farrah.kaiyom@gmail.com>
2018-08-31 15:36:56 -0700

    Climb Button

---
+++ Robot_Code/src/team3647subsystems/ClimbButton.java
@@ -0,0 +1,36 @@
+package team3647subsystems;
+
+public class ClimbButton {
+	
+	public static int buttonState;
+	
+	public static void climb(boolean joyValue)
+	{
+		switch(buttonState)
+		{
+		case 0:
+			if(joyValue)
+			{
+				buttonState = 1;
+			}
+			break;
+		case 1:
+			Wrist.moveToUp();
+			//need elevator zero
+			buttonState = 2;
+			break;
+		case 2:
+			Lock.lock();
+			buttonState = 3;
+			break;
+		case 3:
+			//go to climb height
+			buttonState = 4;
+			break;
+		case 4:
+			Shifter.lowGear();
+			break;
+		}
+	}
+
+}
