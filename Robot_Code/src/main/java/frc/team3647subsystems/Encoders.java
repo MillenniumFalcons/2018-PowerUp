68bfbc4fc38eb0af34fe01bec36f3bdc7cf0a2bf
Jonathan Mi <34979278+jonathanmi6@users.noreply.github.com>
2018-09-11 16:11:23 -0700

    Updated Encoders Class

--- Robot_Code/src/main/java/frc/team3647subsystems/Encoders.java
+++ Robot_Code/src/main/java/frc/team3647subsystems/Encoders.java
@@ -1,25 +1,44 @@
-package frc.team3647subsystems;
+package frc.team3647Subsystems;
+import frc.team3647ConstantsAndFunctions.*;
 
 public class Encoders 
 {
-	public double leftEncoderValue, rightEncoderValue;
+	public int leftEncoderValue, rightEncoderValue, leftEncoderVelocity, rightEncoderVelocity, leftEncoderCLError, rightEncoderCLError;
 	
 	public void setEncoderValues()
 	{
-		leftEncoderValue = Drivetrain.leftSRX.getSensorCollection().getQuadraturePosition();
-		rightEncoderValue = -Drivetrain.rightSRX.getSensorCollection().getQuadraturePosition();
+		leftEncoderValue = Drivetrain.leftSRX.getSelectedSensorPosition(Constants.drivePID);
+		rightEncoderValue = Drivetrain.rightSRX.getSelectedSensorPosition(Constants.drivePID);
+
+		leftEncoderVelocity = Drivetrain.leftSRX.getSelectedSensorVelocity(Constants.drivePID);
+		rightEncoderVelocity = Drivetrain.rightSRX.getSelectedSensorVelocity(Constants.drivePID);
+		
+		leftEncoderCLError = Drivetrain.leftSRX.getClosedLoopError(Constants.drivePID);
+		rightEncoderCLError = Drivetrain.rightSRX.getClosedLoopError(Constants.drivePID);
 	}
 	
 	public void resetEncoders()
 	{
-		Drivetrain.leftSRX.getSensorCollection().setQuadraturePosition(0, 10);
-		Drivetrain.rightSRX.getSensorCollection().setQuadraturePosition(0, 10);
+		Drivetrain.leftSRX.setSelectedSensorPosition(0, Constants.drivePID, Constants.kTimeoutMs);
+		Drivetrain.rightSRX.setSelectedSensorPosition(0, Constants.drivePID, Constants.kTimeoutMs);
 	}
 	
 	public void testEncoders()
 	{
 		System.out.println("Left Encoder Value: " + leftEncoderValue);
 		System.out.println("Right Encoder Value: " + rightEncoderValue);
+	}
+
+	public void testEncoderVelocity()
+	{
+		System.out.println("Left Encoder Velocity: " + leftEncoderVelocity);
+		System.out.println("Right Encoder Velocity: " + rightEncoderVelocity);
+	}
+
+	public void testEncoderCLError()
+	{
+		System.out.println("Left Encoder CL Error: " + leftEncoderCLError);
+		System.out.println("Right Encoder CL Error: " + rightEncoderCLError);
 	}
 
 }
