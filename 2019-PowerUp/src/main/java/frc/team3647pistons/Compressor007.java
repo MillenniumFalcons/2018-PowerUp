0dc25133dfcbccaf9acb39fd2141a594f067855b
Kunal Singla <tekunalogy@gmail.com>
2019-04-22 20:28:40 -0700

    fixed name

--- 2019-PowerUp/src/main/java/frc/team3647pistons/Compressor007.java
+++ 2019-PowerUp/src/main/java/frc/team3647pistons/Compressor007.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj.Compressor;
 
 public class Compressor007 
 {
-	static Compressor c = new Compressor(0);
+	static Compressor compressor = new Compressor(0);
 	
 	public static void runCompressor()
 	{
-	    c.setClosedLoopControl(true);
+	    compressor.setClosedLoopControl(true);
 	}
 }
